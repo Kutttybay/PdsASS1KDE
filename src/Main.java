da1cd946541646e9507a4e4e468b4b80b3c20010
kutttybay <kutttybay@gmail.com>
2023-10-04 23:40:06 +0600

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Configuration config = Configuration.getInstance();
@@ -8,7 +11,19 @@ public class Main {
         // Choose a counting strategy (SimpleWordCountStrategy or CharacterCountStrategy)
         WordCountStrategy wordCountStrategy = new SimpleWordCountStrategy();
 
-        int wordCount = wordCountStrategy.countWords(text);
+        // Generate a random number between 1 and 100 as the text length
+        int randomTextLength = new Random().nextInt(100) + 1;
+        StringBuilder randomText = new StringBuilder();
+
+        // Generate random characters for the text
+        for (int i = 0; i < randomTextLength; i++) {
+            char randomChar = (char) (new Random().nextInt(26) + 'a');
+            randomText.append(randomChar);
+        }
+
+        System.out.println("Random text: " + randomText.toString());
+
+        int wordCount = wordCountStrategy.countWords(randomText.toString());
         System.out.println("Word count: " + wordCount);
     }
 }
